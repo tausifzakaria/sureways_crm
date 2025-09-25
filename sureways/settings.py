d9e33d72df43e0a1940e8e33bf196aeedec20472
tausifzakaria <zakariatausif2@gmail.com>
2025-09-25 17:24:52 +0530

    Production Fix

--- sureways/settings.py
+++ sureways/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
